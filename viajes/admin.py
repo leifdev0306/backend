6ee7380e2e06bcc60165c9536e4751fa5f0b3398
leifdev0306 <palasatenea0306@gmail.com>
2026-06-19 07:53:48 -0400

    add provinces

--- viajes/admin.py
+++ viajes/admin.py
@@ -1,25 +1,46 @@
 from django.contrib import admin
-from .models import Entidad, Gestor, Viaje, Pasajero, LiquidacionMensual
+from .models import Provincia, Entidad, Gestor, Viaje, Pasajero, Puntuacion, LiquidacionMensual
 
+# Registrar el modelo Provincia para que aparezca en el admin
+@admin.register(Provincia)
+class ProvinciaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre')
+    search_fields = ('nombre',)
+    ordering = ('nombre',)
+
+# Registrar el resto de modelos (opcional pero recomendado)
 @admin.register(Entidad)
 class EntidadAdmin(admin.ModelAdmin):
-    list_display = ['nombre', 'telefono', 'comision_porcentaje', 'suspendida']
-    list_editable = ['suspendida']
+    list_display = ('id', 'nombre', 'telefono', 'suspendida', 'puntuacion_promedio')
+    list_filter = ('suspendida',)
+    search_fields = ('nombre', 'telefono')
 
 @admin.register(Gestor)
 class GestorAdmin(admin.ModelAdmin):
-    list_display = ['user', 'entidad']
+    list_display = ('id', 'user', 'entidad')
+    list_filter = ('entidad',)
+    search_fields = ('user__username', 'user__email')
 
 @admin.register(Viaje)
 class ViajeAdmin(admin.ModelAdmin):
-    list_display = ['id', 'entidad', 'origen', 'destino', 'fecha', 'hora', 'estado', 'cupos_disponibles']
-    list_filter = ['estado', 'entidad']
+    list_display = ('id', 'entidad', 'origen', 'destino', 'fecha', 'hora', 'estado', 'cupos_disponibles')
+    list_filter = ('entidad', 'origen', 'destino', 'estado')
+    search_fields = ('entidad__nombre', 'origen__nombre', 'destino__nombre', 'matricula')
+    readonly_fields = ('creado',)
 
 @admin.register(Pasajero)
 class PasajeroAdmin(admin.ModelAdmin):
-    list_display = ['viaje', 'nombre_completo', 'ci', 'reservado_en']
+    list_display = ('id', 'viaje', 'nombre_completo', 'ci', 'reservado_en')
+    list_filter = ('viaje__entidad',)
+    search_fields = ('nombre_completo', 'ci')
+
+@admin.register(Puntuacion)
+class PuntuacionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'viaje', 'ci_cliente', 'valor', 'creado')
+    list_filter = ('viaje__entidad', 'valor')
+    search_fields = ('ci_cliente',)
 
 @admin.register(LiquidacionMensual)
-class LiquidacionAdmin(admin.ModelAdmin):
-    list_display = ['entidad', 'año', 'mes', 'ingresos_totales', 'comision', 'pagada']
-    list_editable = ['pagada']
+class LiquidacionMensualAdmin(admin.ModelAdmin):
+    list_display = ('id', 'entidad', 'año', 'mes', 'ingresos_totales', 'comision', 'pagada')
+    list_filter = ('entidad', 'año', 'mes', 'pagada')
