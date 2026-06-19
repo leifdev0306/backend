a8f9ad2bfa6fe7492afe8c4b28c72a7870e7a1d6
leifdev0306 <palasatenea0306@gmail.com>
2026-06-19 07:16:55 -0400

    static files update

--- build.sh
+++ build.sh
@@ -2,6 +2,12 @@
 set -o errexit
 
 pip install -r requirements.txt
+
+# Crear directorio para archivos estáticos (si no existe)
+mkdir -p staticfiles
+
+# Recolectar archivos estáticos (incluye los del admin)
 python manage.py collectstatic --noinput
+
 python manage.py makemigrations
 python manage.py migrate
